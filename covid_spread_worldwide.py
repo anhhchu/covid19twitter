5b240f80345ae57ab38a7da54e240b9a4b588127
AnhHChu <13889581+anhhchu@users.noreply.github.com>
2021-04-03 15:31:05 -0500

    Create covid_spread_worldwide.py

---
+++ covid_spread_worldwide.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+"""
+Create a new aggregation for covid 19 spread worldwide based on bigquery-public-data.covid19_ecdc dataset 
+"""
+
+from google.cloud import bigquery
+from google.cloud.bigquery import job
+from google.cloud.bigquery.table import *
+
+# Create a new Google BigQuery client using Google Cloud Platform project
+# defaults.
+bq = bigquery.Client()
+
+# Create a new BigQuery dataset.
+dataset_ref = bq.dataset("covid_spread_worldwide")
+dataset = bigquery.Dataset(dataset_ref)
+dataset = bq.create_dataset(dataset)
+
+# In the new BigQuery dataset, create a new table.
+table_ref = dataset.table('monthly')
+# The table needs a schema before it can be created and accept data.
+# We create an ordered list of the columns using SchemaField objects.
+schema = []
+schema.append(SchemaField("countries_and_territories", "string"))
+schema.append(SchemaField("year", "integer"))
+schema.append(SchemaField("month", "integer"))
+schema.append(SchemaField("total_cases", "integer"))
+schema.append(SchemaField("total_deaths", "integer"))
+
+# We assign the schema to the table and create the table in BigQuery.
+table = bigquery.Table(table_ref, schema=schema)
+table = bq.create_table(table)
+
+# Next, we issue a query in StandardSQL.
+# The query selects the fields of interest.
+query = """
+SELECT distinct countries_and_territories, year, month, sum(daily_confirmed_cases) as total_cases, sum(daily_deaths) as total_deaths
+ FROM `bigquery-public-data.covid19_ecdc.covid_19_geographic_distribution_worldwide`
+ group by countries_and_territories, year, month
+"""
+
+# We create a query job to save the data to a new table.
+job_config = bigquery.QueryJobConfig()
+job_config.write_disposition = 'WRITE_TRUNCATE'
+job_config.destination = table_ref
+
+# 'Submit the query.
+query_job = bq.query(query, job_config=job_config)
